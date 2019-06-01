5dcfc3903cb7e2d8766dd7f4185968fdabb8164e
Ferror <zmalcherczyk@gmail.com>
2019-06-02 01:40:58 +0200

    more test dump

--- src/Application/Controller/MailController.php
+++ src/Application/Controller/MailController.php
@@ -40,7 +40,7 @@ final class MailController extends SymfonyController
 //        dump($request->getHttpHost());
 //        dump($request->headers->get('referer'));
 //        dump($request->headers);
-        dump($request);
+        var_dump($request->getClientIp());
 
         if (!$this->isOnWhitelist($request->getHost())) {
             throw new HostNotWhitelisted();
