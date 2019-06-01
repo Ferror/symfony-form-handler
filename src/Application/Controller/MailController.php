78016fc8ecef662a90d27e4c1cd8b520f562be18
Ferror <zmalcherczyk@gmail.com>
2019-06-02 01:03:08 +0200

    create whitelisting hosts and saving request to json file

--- src/Application/Controller/MailController.php
+++ src/Application/Controller/MailController.php
@@ -3,17 +3,29 @@ declare(strict_types=1);
 
 namespace Application\Controller;
 
+use Application\Exception\HostNotWhitelisted;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Controller\SymfonyController;
 
 final class MailController extends SymfonyController
 {
     /**
-     * @Route("/{uuid}", methods={"POST", "GET"})
+     * @Route("/", methods={"GET"})
      */
-    public function mail(Request $request, string $uuid) : JsonResponse
+    public function index() : Response
+    {
+        return new JsonResponse(['OK']);
+    }
+
+
+    /**
+     * @Route("/{uuid}", methods={"POST"})
+     */
+    public function mail(Request $request, string $uuid) : Response
     {
         //request posiada hosta, uuid z urla i form values
 
@@ -24,6 +36,15 @@ final class MailController extends SymfonyController
             ]);
         }
 
+        if (!$this->isOnWhitelist($request->getHost())) {
+            throw new HostNotWhitelisted();
+        }
+
+        $fileName = 'form' . random_int(0, 100) . '.json';
+        $file = fopen($this->getParameter('kernel.project_dir') . '/data/content/' . $fileName , 'w');
+        fwrite($file, json_encode($request->request->all()));
+        fclose($file);
+
 //        $message = (new \Swift_Message())
 //            ->setSubject('Hello Email')
 //            ->setFrom('send@example.com')
@@ -31,9 +52,23 @@ final class MailController extends SymfonyController
 //            ->setBody($this->renderView('email.html.twig'), 'text/html');
 //        $this->getSwiftMailer()->send($message);
 
+        if ($request->get('_next')) {
+            return new RedirectResponse(
+                $request->get('_next'),
+                302
+            );
+        }
+
         return new JsonResponse([
             'data' => $request->request->all(),
             'host' => $request->getHost(),
         ]);
+    }
+
+    private function isOnWhitelist(string $host) : bool
+    {
+        $file = file_get_contents($this->getParameter('kernel.project_dir') . '/data/whitelist.json');
+
+        return in_array($host, json_decode($file, true), true);
     }
 }
